fdea5ceaeb547f6d318405ff1560f8546807c784
SURFACEPRO9\mitsu <mitsuhiro.seino@gmail.com>
2026-04-26 00:29:03 +0900

    prefix

--- src/createSassClassNameFunction/createSassClassNameFunction.ts
+++ src/createSassClassNameFunction/createSassClassNameFunction.ts
@@ -1,5 +1,6 @@
 import type { Value } from 'sass';
 import { SassString } from 'sass';
+import type { CreateSassClassNameFunctionOptions } from './types';
 
 /**
  * TypeScriptで定義したCSSのクラス名をsassに反映するカスタム関数
@@ -8,9 +9,11 @@ import { SassString } from 'sass';
  */
 export default function createSassClassNameFunction(
   classNames: Record<string, string>,
+  options: CreateSassClassNameFunctionOptions = {},
 ) {
+  const { prefix = '', suffix = '' } = options;
   return (args: Value[]) => {
     const name = (args[0] as SassString).text;
-    return new SassString(classNames[name]);
+    return new SassString(classNames[prefix + name + suffix]);
   };
 }
---
+++ src/createSassClassNameFunction/types.ts
@@ -0,0 +1,4 @@
+export type CreateSassClassNameFunctionOptions = {
+  prefix?: string;
+  suffix?: string;
+};
